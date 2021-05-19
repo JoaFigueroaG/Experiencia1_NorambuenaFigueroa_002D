89fbb86419f993fcd49bf91a10135ab6add99617
Joa Figueroa <joa.figueroag@duocuc.cl>
2021-05-18 22:02:21 -0400

    Se implementan los JS y los scripts en las paginas

---
+++ js/Funciones.js
@@ -0,0 +1,6 @@
+
+function nombrar()
+{
+    var nom=prompt('Ingrese su nombre por favor.');
+    alert('Muchas gracias por su comentario '+nom+', su aporte ayuda a mejorar la pagina cada dia mas');
+}
